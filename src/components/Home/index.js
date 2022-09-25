41fcafc7de063446cb8b8cf7a48e9bd781cd2f7e
Mateusz Pietrzak <pietrzakm002@gmail.com>
2022-09-26 01:14:54 +0200

    Creating: movies

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 import ImageSlider from "../ImageSlider";
 import data from "./data";
 import Viewers from "../Viewers";
+import Movies from "../Movies";
 
 function Home() {
   return (
     <Container>
       <ImageSlider data={data} />
       <Viewers />
+      <Movies />
     </Container>
   );
 }
---
+++ src/components/Movies/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function index() {
+  return (
+    <Container>
+      <h4>Recommended for You</h4>
+    </Container>
+  );
+}
+
+const Container = styled.div``;
+
+export default index;
